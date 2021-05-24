79ef2cfc9637543d4c72448463adde5abbf080dc
wcyxpy <55188590+wcyxpy@users.noreply.github.com>
2021-05-24 19:34:02 +0800

    5/24
    1

--- application/index/controller/Student.php
+++ application/index/controller/Student.php
@@ -40,10 +40,10 @@ class Student extends Controller//权限1
             ->where('username', $usrname)
             ->find();
         $this->assign('data', $result);
-        $this->assign('data1','visible' );
-        if ($result['jurisdiction']==10){
-            $this->assign('data1','hidden' );
-        }
+//        $this->assign('data1','visible' );
+//        if ($result['jurisdiction']==10){
+//            $this->assign('data1','hidden' );
+//        }
         return $this->fetch();
     }
 
@@ -54,10 +54,10 @@ class Student extends Controller//权限1
             ->where('username', $usrname)
             ->find();
         $this->assign('data', $result);
-        $this->assign('data1','visible' );
-        if ($result['jurisdiction']==10){
-            $this->assign('data1','hidden' );
-        }
+//        $this->assign('data1','visible' );
+//        if ($result['jurisdiction']==10){
+//            $this->assign('data1','hidden' );
+//        }
         return $this->fetch();
     }
 
